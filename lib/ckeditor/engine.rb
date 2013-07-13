48c07784306c56b2584a4cf2baca51f79c94aee7
Grzegorz Brzezinka <info@matfiz.com.pl>
2013-07-13 08:41:58 +0200

    form 2.0

--- lib/ckeditor/engine.rb
+++ lib/ckeditor/engine.rb
@@ -12,14 +12,14 @@ module Ckeditor
       ActiveSupport.on_load :action_view do
         ActionView::Base.send :include, Ckeditor::Helpers::ViewHelper
         ActionView::Base.send :include, Ckeditor::Helpers::FormHelper
-        #ActionView::Helpers::FormBuilder.send :include, Ckeditor::Helpers::FormBuilder
-	require "ckeditor/hooks/formtastic"
+        ActionView::Helpers::FormBuilder.send :include, Ckeditor::Helpers::FormBuilder
       end
     end
     
     initializer "ckeditor.hooks" do
       if Object.const_defined?("Formtastic")
-        ::Formtastic::SemanticFormBuilder.send :include, Ckeditor::Hooks::FormtasticBuilder
+        #::Formtastic::SemanticFormBuilder.send :include, Ckeditor::Hooks::FormtasticBuilder
+        require "ckeditor/hooks/formtastic"
       end
       
       if Object.const_defined?("SimpleForm")
